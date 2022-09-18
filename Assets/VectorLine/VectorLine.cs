addf30fd0d949ec6e40f195c728255b15871f721
Carlo Piersanti <cpi@obvious.tech>
2022-09-18 12:29:54 +0200

    Checkpoint

--- Assets/VectorLine/VectorLine.cs
+++ Assets/VectorLine/VectorLine.cs
@@ -13,32 +13,50 @@ public class VectorLine : MonoBehaviour
         Vector2[] linepoints = new Vector2[]
         {
             new Vector2(-1,0),
-            new Vector2(1,0)
+            new Vector2(1,0),
+            new Vector2(3,3)
         };
 
         mesh = new Mesh();
-        mesh.vertices = new Vector3[]
+
+        Vector3[] vertices = new Vector3[4 * (linepoints.Length - 1)];
+
+        for ( int i = 0; i< linepoints.Length -1; i++)
         {
-            new Vector3(-1,0,0),
-            new Vector3(1,0,0),
-            new Vector3(1,0,0),
-            new Vector3(-1,0,0),
-        };
-        /*
-        mesh.SetUVs(0, new Vector3[] {
-            new Vector3(0,-1,0),
-            new Vector3(0,-1,0),
-            new Vector3(0,1,0),
-            new Vector3(0,1,0)
-        });
-        */
-        mesh.SetUVs(0, new Vector3[] {
-            new Vector3(1,0,0),
-            new Vector3(1,0,0),
-            new Vector3(-1,0,0),
-            new Vector3(-1,0,0)
-        });
-        mesh.triangles = new int[] { 1, 0, 2, 3, 2, 0 };
+            vertices[4 * i] = new Vector3(linepoints[i].x, linepoints[i].y, 0);
+            vertices[4 * i + 1] = new Vector3(linepoints[i+1].x, linepoints[i + 1].y, 0);
+            vertices[4 * i + 2] = new Vector3(linepoints[i + 1].x, linepoints[i + 1].y, 0);
+            vertices[4 * i + 3] = new Vector3(linepoints[i].x, linepoints[i].y, 0);
+
+        }
+
+        mesh.vertices = vertices;
+
+        Vector3 [] uvs = new Vector3[4 * (linepoints.Length - 1)];
+        for (int i = 0; i < linepoints.Length - 1; i++)
+        {
+            uvs[4 * i] = linepoints[i+1] - linepoints[i];
+            uvs[4 * i + 1] = linepoints[i + 1] - linepoints[i];
+            uvs[4 * i + 2] = linepoints[i] - linepoints[i+1];
+            uvs[4 * i + 3] = linepoints[i] - linepoints[i + 1];
+        }
+
+        
+        mesh.SetUVs(0, uvs);
+
+        int[] triangles = new int[6 * (linepoints.Length-1)];
+        for (int i=0; i< linepoints.Length - 1; i++)
+        {
+            triangles[i * 6 ] = i * 4 + 1;
+            triangles[i * 6 + 1] = i * 4;
+            triangles[i * 6 + 2] = i * 4 + 2;
+            triangles[i * 6 + 3] = i * 4 + 3;
+            triangles[i * 6 + 4] = i * 4 + 2;
+            triangles[i * 6 + 5] = i * 4;
+        }
+
+
+        mesh.triangles = triangles;
 
         GetComponent<MeshFilter>().mesh = mesh;
     }
