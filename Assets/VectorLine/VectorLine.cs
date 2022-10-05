8dd1cc6018789e4080d481ca721eb1b076bd9b55
Carlo Piersanti <cpi@obvious.tech>
2022-10-05 22:17:02 +0200

    Bug fixes

--- Assets/VectorLine/VectorLine.cs
+++ Assets/VectorLine/VectorLine.cs
@@ -37,40 +37,40 @@ public class VectorLine : MonoBehaviour
         Vector3[] uvs = new Vector3[5 * (linepoints.Count - 1)];
         for (int i = 0; i < linepoints.Count - 1; i++)
         {
-            uvs[5 * i] = (linepoints[i + 1] - linepoints[i]).normalized;
-            uvs[5 * i + 1] = (linepoints[i + 1] - linepoints[i]).normalized;
-            uvs[5 * i + 2] = (linepoints[i] - linepoints[i + 1]).normalized;
-            uvs[5 * i + 3] = (linepoints[i] - linepoints[i + 1]).normalized;
+            uvs[5 * i] = (linepoints[i + 1] - linepoints[i]);
+            uvs[5 * i + 1] = (linepoints[i + 1] - linepoints[i]);
+            uvs[5 * i + 2] = (linepoints[i] - linepoints[i + 1]);
+            uvs[5 * i + 3] = (linepoints[i] - linepoints[i + 1]);
             uvs[5 * i + 4] = Vector3.zero;
         }
 
 
         mesh.SetUVs(0, uvs);
 
-        int[] triangles = new int[9 * (linepoints.Count - 1)];
+        int[] triangles = new int[12 * (linepoints.Count - 1)];
         for (int i = 0; i < linepoints.Count - 1; i++)
         {
-            triangles[i * 9] = i * 5 + 1;
-            triangles[i * 9 + 1] = i * 5;
-            triangles[i * 9 + 2] = i * 5 + 2;
-            triangles[i * 9 + 3] = i * 5 + 3;
-            triangles[i * 9 + 4] = i * 5 + 2;
-            triangles[i * 9 + 5] = i * 5;
+            triangles[i * 12] = i * 5 + 1;
+            triangles[i * 12 + 1] = i * 5;
+            triangles[i * 12 + 2] = i * 5 + 2;
+            triangles[i * 12 + 3] = i * 5 + 3;
+            triangles[i * 12 + 4] = i * 5 + 2;
+            triangles[i * 12 + 5] = i * 5;
         }
 
         for (int i = 1; i < linepoints.Count - 1; i++)
         {
-            if (Vector2.SignedAngle(linepoints[i - 1] - linepoints[i], linepoints[i + 1] - linepoints[i]) < 0)
+            //if (Vector2.SignedAngle(linepoints[i - 1] - linepoints[i], linepoints[i + 1] - linepoints[i]) < 0)
             {
-                triangles[(i - 1) * 9 + 6] = (i - 1) * 5 + 4;
-                triangles[(i - 1) * 9 + 7] = (i - 1) * 5 + 5;
-                triangles[(i - 1) * 9 + 8] = (i - 1) * 5 + 1;
+                triangles[(i - 1) * 12 + 6] = (i - 1) * 5 + 4;
+                triangles[(i - 1) * 12 + 7] = (i - 1) * 5 + 5;
+                triangles[(i - 1) * 12 + 8] = (i - 1) * 5 + 1;
             }
-            else
+            //else
             {
-                triangles[(i - 1) * 9 + 6] = (i - 1) * 5 + 4;
-                triangles[(i - 1) * 9 + 7] = (i - 1) * 5 + 2;
-                triangles[(i - 1) * 9 + 8] = (i - 1) * 5 + 8;
+                triangles[(i - 1) * 12 + 9] = (i - 1) * 5 + 4;
+                triangles[(i - 1) * 12 + 10] = (i - 1) * 5 + 2;
+                triangles[(i - 1) * 12 + 11] = (i - 1) * 5 + 8;
             }
         }
 
