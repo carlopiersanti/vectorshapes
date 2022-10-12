c0abb6a3ef8bbcede5440ddd23ad684c36b7d93d
Carlo Piersanti <cpi@obvious.tech>
2022-10-12 13:36:27 +0200

    Checkpoint

--- Assets/VectorLine/VectorLineUI.cs
+++ Assets/VectorLine/VectorLineUI.cs
@@ -25,28 +25,36 @@ public class VectorLineUI : MonoBehaviour
 
     Vector3 lastMousePosition = new Vector3(float.NaN, float.NaN, float.NaN);
     int id = 0;
+    int blacklistedId = -2;
 
     private void Update()
     {
         int[] data = new int[1];
         collisionBuffer.GetData(data);
         collisionBuffer.SetData(new int[] { -1 });
-        material.SetInteger("selectedIdentifier", data[0]);
         material.SetVector("mousePosition", Input.mousePosition);
-        if (Input.GetMouseButton(0) && Input.mousePosition != lastMousePosition)
+        if (Input.GetMouseButtonDown(0))
+        {
+            blacklistedId = id;
+            vectorLine = GameObject.Instantiate(vectorLineBase.gameObject).GetComponent<VectorLine>();
+            vectorLine.SetId(id++);
+            lastMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0) && Vector3.Distance(Input.mousePosition, lastMousePosition) > 4 )
         {
             lastMousePosition = Input.mousePosition;
             Ray r = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
             if (Physics.Raycast(r, out var raycastHit))
             {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    vectorLine = GameObject.Instantiate(vectorLineBase.gameObject).GetComponent<VectorLine>();
-                    vectorLine.SetId(id++);
-                }
                 vectorLine.AddPoint(raycastHit.point);
             }
         }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            blacklistedId = -2;
+        }
+
+        material.SetInteger("selectedIdentifier", blacklistedId == data[0] ? -1 : data[0]);
     }
 }
 }
